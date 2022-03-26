370b1de038821eb3a9079b7179f6658c41f610a9
Avihalely <35514156+Avihalely@users.noreply.github.com>
2022-03-26 21:43:55 +0300

    added changes to pipelines.py and settings.py files

--- article_crawler/article_crawler/pipelines.py
+++ article_crawler/article_crawler/pipelines.py
@@ -5,7 +5,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+# each tesks displays here as an class
+# display all our data as an python object
+from scrapy.exceptions import DropItem
+from datetime import datetime
 
-class ArticleCrawlerPipeline:
-    def process_item(self, item, spider):
-        return item
+
+class CheckItemPipeline:
+    def process_item(self, article, spider):
+        if not article['lastUpdated'] or not article['url'] or not article['title']:
+            raise DropItem('missing something')
+        return article
+
+
+class CleanDatePipline:
+    def process_item(self, article, spider):
+        article['lastUpdated'].replace('this page was last edited on ', '').strip()
+        article['lastUpdated'] = datetime.strptime(article['lastUpdated'], '%d %B %Y, at %H%M')
+        return article
